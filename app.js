921cf4c9665e660f4db36059b7db1a4a1264e10b
Lucio Franco <lucio.franco@du.edu>
2016-06-14 11:24:20 -0600

    added reading messages

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
+'use strict'
+
 // variable setup
 var Botkit = require('botkit');
 var slack = require('slack');
+var parse = require('./parse');
 
 var controller = Botkit.slackbot({
   debug: false,
 });
 
+var token = "xoxb-50511748432-Rztd1yjENcRHpJ9llAfMQbpd";
+
 var bot = controller.spawn({
-	token: process.env.TOKEN
+  token
 }).startRTM();
 
 controller.on('direct_mention', (bot, message) => {
-  console.log(message);
-})
+  let { channel, text } = message;
+
+  parse(text);
+
+  slack.channels.history({token, channel}, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+
+    console.log(data);
+  });
+});
