9384f9e5769cc93b6152df87c2ebde192b185691
Alex Luo <alex.luo@readytalk.com>
2016-06-14 13:30:41 -0600

    switched to using only the slack API, got rid of botkit use.

--- app.js
+++ app.js
@@ -1,30 +1,24 @@
 'use strict'
 
 // variable setup
-var Botkit = require('botkit');
 var slack = require('slack');
 var parse = require('./parse');
+//var token = "xoxb-50511748432-Rztd1yjENcRHpJ9llAfMQbpd";
+var bot = slack.rtm.client();
+var token = process.env.token;
+bot.listen({token:token});
 
-var controller = Botkit.slackbot({
-  debug: false,
-});
 
-var token = "xoxb-50511748432-Rztd1yjENcRHpJ9llAfMQbpd";
-
-var bot = controller.spawn({
-  token
-}).startRTM();
-
-controller.on('direct_mention', (bot, message) => {
-  let { channel, text } = message;
-
+bot.message((message) => {
+  let { channel, text, user } = message;
   parse(text);
-
+  if (user != bot){
+    console.log(text);
+  }
   slack.channels.history({token, channel}, (err, data) => {
     if (err) {
       console.log(err);
     }
-
-    console.log(data);
+   
   });
 });
