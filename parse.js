4f88464811ffc27fb00fdd5d19f08b065ee9f46c
Lucio Franco <lucio.franco@du.edu>
2016-06-15 12:05:17 -0600

    added date extractor and tests for parser

--- parse.js
+++ parse.js
@@ -1,5 +1,42 @@
+var moment = require('moment');
+
 module.exports = (text) => {
-  return new Promise((resolve, result) => {
-    resolve();
+  return new Promise((resolve) => {
+    let ast = {
+      keywords: [],
+      mentions: false,
+      links: false,
+      date: null
+    };
+
+    let date = matchDate(text);
+    text = text.replace(date.regex, '');
+    ast.date = date.date;
+
+    resolve(ast);
   });
+};
+
+function matchDate(text) {
+  const regexes = [
+    [/([0-9])\s(?:hour|hours|h|hr)/i, (res) => moment().subtract(res[1], 'hours')],
+    [/past\s(?:hour|hours|h|hr)/i, (res) => moment().subtract(1, 'hours')],
+    [/([0-9])\s(?:day|days|d)/i, (res) => moment().subtract(res[1], 'days')],
+    [/past\s(?:day|days|d)/i, (res) => moment().subtract(1, 'days')],
+    [/([0-9])\s(?:week|weeks|w)/i, (res) => moment().subtract(res[1], 'weeks')],
+    [/past\s(?:week|weeks|w)/i, (res) => moment().subtract(1, 'weeks')]
+  ];
+
+  for(let r of regexes) {
+    let res = r[0].exec(text);
+    if (res != null) {
+      return {
+        date: r[1](res),
+        regex: r[0]
+      };
+    }
+  }
+
+
+  return null;
 }
---
+++ parse.test.js
@@ -0,0 +1,61 @@
+var should = require('should');
+var parse = require('./parse');
+var moment = require('moment');
+
+describe('Parser Tests', () => {
+  describe('Date Extractor', () => {
+    describe('Hour', () => {
+      const current_hour = moment().subtract(1, 'hours').hours();
+
+      it('hour with number', () => {
+        return parse('foxden 1 hour')
+          .then((result) => {
+            result.date.hours().should.equal(current_hour);
+          });
+      });
+
+      it('hour with past keyword', () => {
+        return parse('foxden past hour')
+          .then((result) => {
+            result.date.hours().should.equal(current_hour);
+          });
+      });
+    });
+
+    describe('Day', () => {
+      const current_day = moment().subtract(1, 'days').days();
+
+      it('day with number', () => {
+        return parse('foxden 1 day')
+          .then((result) => {
+            result.date.days().should.equal(current_day);
+          });
+      });
+
+      it('day with past keyword', () => {
+        return parse('foxden past day')
+          .then((result) => {
+            result.date.days().should.equal(current_day);
+          });
+      });
+    });
+
+    describe('Week', () => {
+      const current_week = moment().subtract(1, 'weeks').days();
+
+      it('week with number', () => {
+        return parse('foxden 1 week')
+          .then((result) => {
+            result.date.days().should.equal(current_week);
+          });
+      });
+
+      it('week with past keyword', () => {
+        return parse('foxden past week')
+          .then((result) => {
+            result.date.days().should.equal(current_week);
+          });
+      });
+    });
+  });
+});
