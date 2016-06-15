2da2cc4c8b7a5de2c281481e3f1e9330891a086f
Alex Luo <alex.luo@readytalk.com>
2016-06-15 11:22:12 -0600

    String now does not include bot mention and is just keywords

--- app.js
+++ app.js
@@ -22,37 +22,43 @@ function getHandle(user) {
   })
 };
 
-/*slack.im.open({token, user: "U1EH544TV"}, (err, data) => {
-  console.log(data);
 
-  slack.chat.postMessage({token, channel: data.channel.id, text: "blah"}, (a, data) => console.log(data));
+// introduction function.
+/*bot.started(function() {
+   slack.im.open({token, user: "U1EH544TV"}, (err, data) => {
+        slack.chat.postMessage({token, channel: data.channel.id, username: "recaptain", text: "Welcome to recaptain!", icon_url: "https://avatars.slack-edge.com/2016-06-13/50511039062_3e2a383deda13028950f_32.png"}, (a, data) => {
+        console.log("@@@");
+})
+      
+})
+});*/
 
-})*/
 
 bot.message((message) => {
   let { channel, text, user, username } = message;
   parse(text)
     .then((result) => {
-      // if first word is recap, respond and get channel history
+      console.log("promise initiatied");
+      // see if message is bot mention
       if(GetFirstWord(text) == "<@U1GF1N0CQ>:"){
-        console.log("message recieve");
+        //Get string without bot mention
+        var myString = text;
+        myString = myString.replace('<@U1GF1N0CQ>: ','');   
+        console.log(myString);
+        //Open IM if there isn't already one
         slack.im.open({token, user}, (err, data) => {
           channel = data.channel.id;
           username = "recaptain";
-          slack.chat.postMessage({token, channel, username, icon_url: "https://avatars.slack-edge.com/2016-06-13/50511039062_3e2a383deda13028950f_32.png", text: "It's Lit"}, (a, data) => 
-                                 console.log("@"+ getHandle(user) + ": " + text));
+          // Reply posts go here based on what they ask for. 
+          slack.chat.postMessage({token, channel, username, icon_url: "https://avatars.slack-edge.com/2016-06-13/50511039062_3e2a383deda13028950f_32.png", 
+            text: "whatever"}, (a, data) => 
+                   console.log("@"+ getHandle(user) + ": " + text));
         })
-        /*slack.im.history({token, channel}, (err, data) => {
-         if (err) {
-         console.log(err);
-         }
-
-         console.log(data);
-         });*/
       };
     })
     .catch((err) => {
       // handle parse error
+        console.log(err);
     });
 });
 
