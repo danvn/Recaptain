b36426f31a9ad99e7404521fc78a2e88f6391dcd
Lucio Franco <lucio.franco@du.edu>
2016-06-15 09:20:14 -0600

    moved parse to promises

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ var token = process.env.token;
 bot.listen({token:token});
 
 //function to check if 1st word is recap
-function GetFirstWord(str){
+function GetFirstWord(str) {
     if (str.indexOf(' ') == -1)
         return str;
     else
         return str.substr(0, str.indexOf(' '));
 };
 
-function getHandle(user){
+function getHandle(user) {
   slack.users.info({token, user}, (err,data) => {
     return data.name;
   })
@@ -31,23 +31,28 @@ function getHandle(user){
 
 bot.message((message) => {
   let { channel, text, user, username } = message;
-  parse(text);
-  // if first word is recap, respond and get channel history
-  if(GetFirstWord(text) == "<@U1GF1N0CQ>:"){
-    console.log("message recieve");
-    slack.im.open({token, user}, (err, data) => {
-        channel = data.channel.id;
-        username = "recaptain";
-        slack.chat.postMessage({token, channel, username, icon_url: "https://avatars.slack-edge.com/2016-06-13/50511039062_3e2a383deda13028950f_32.png", text: "It's Lit"}, (a, data) => 
-          console.log("@"+ getHandle(user) + ": " + text));
-})
-    /*slack.im.history({token, channel}, (err, data) => {
-    if (err) {
-      console.log(err);
-    }
+  parse(text)
+    .then((result) => {
+      // if first word is recap, respond and get channel history
+      if(GetFirstWord(text) == "<@U1GF1N0CQ>:"){
+        console.log("message recieve");
+        slack.im.open({token, user}, (err, data) => {
+          channel = data.channel.id;
+          username = "recaptain";
+          slack.chat.postMessage({token, channel, username, icon_url: "https://avatars.slack-edge.com/2016-06-13/50511039062_3e2a383deda13028950f_32.png", text: "It's Lit"}, (a, data) => 
+                                 console.log("@"+ getHandle(user) + ": " + text));
+        })
+        /*slack.im.history({token, channel}, (err, data) => {
+         if (err) {
+         console.log(err);
+         }
 
-    console.log(data);
-    });*/
-  };
+         console.log(data);
+         });*/
+      };
+    })
+    .catch((err) => {
+      // handle parse error
+    });
 });
 
