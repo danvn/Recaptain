52aa38dea5a963d3f28029bcedda584b41f3290a
Lucio Franco <lucio.franco@du.edu>
2016-06-17 11:54:00 -0600

    added basic keyword module and fixed mentions module

--- modules.js
+++ modules.js
@@ -3,13 +3,21 @@ var _ = require('lodash');
 exports.mentions = (messages, message, ast) => {
   return new Promise((resolve, reject) => {
     resolve(_.filter(messages, (e) => {
-        return e.text.contains(message.user);
+        return e.text.includes(message.user);
     }));
   });
 };
 
 exports.keyword = (messages, message, ast) => {
   return new Promise((resolve, reject) => {
-    resolve('keywords');
+    resolve(_.chain(messages)
+            .filter((e) => {
+              return _.reduce(ast.keywords, (exists, e) => {
+                return !exists ? e.text.includes(e) : true;
+              }, false);
+            })
+            .take(5)
+            .value()
+           );
   });
 };
