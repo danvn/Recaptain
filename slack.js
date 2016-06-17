8f0e48093c89dadb4c80cdd69f253b251e6bf9b5
Alex Luo <alex.luo@readytalk.com>
2016-06-17 11:36:56 -0600

    moved all slack operations to slack.js

--- slack.js
+++ slack.js
@@ -11,3 +11,29 @@ exports.history = (channel) => {
 });
 };
 
+exports.im = (token, user) => {
+    return new Promise((resolve, reject) => {
+    slack.im.open({token, user}, (err, data) => {
+        resolve(data);
+    })
+});
+};
+
+exports.post = (token, channel, text, icon, username, attach) => {
+    return new Promise((resolve, reject) => {
+     console.log("in post before slack post");
+     slack.chat.postMessage({token, channel, text, icon_url: icon, username, attachments: attach}, (err, data) => {
+        console.log("in post");
+        resolve();
+    });
+});   
+};
+
+exports.userdata = (token, user) => {
+    return new Promise((resolve, reject) => {
+    console.log("OUTSIDE SLACK USER FUNCTION " + user);
+    slack.users.info({token, user}, (err, data) => {
+        resolve(data);
+    });
+});
+};
