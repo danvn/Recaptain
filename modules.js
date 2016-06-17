0b2027828cd5fae4129366f5e35b3fc4e01ade6e
Lucio Franco <lucio.franco@du.edu>
2016-06-17 13:21:39 -0600

    cleaned up modules

--- modules.js
+++ modules.js
@@ -2,21 +2,23 @@ var _ = require('lodash');
 
 exports.mentions = (messages, message, ast) => {
   return new Promise((resolve, reject) => {
-    resolve(_.filter(messages, (e) => {
-        return e.text.includes(message.user);
-    }));
+    resolve(_.chain(messages)
+            .filter((e) => e.text.includes(message.user.id))
+            .take(5)
+            .map((e) => e.username + ': ' + e.text)
+            .join('\n')
+            .value()
+           );
   });
 };
 
 exports.keyword = (messages, message, ast) => {
   return new Promise((resolve, reject) => {
     resolve(_.chain(messages)
-            .filter((e) => {
-              return _.reduce(ast.keywords, (exists, e) => {
-                return !exists ? e.text.includes(e) : true;
-              }, false);
-            })
+            .filter((e) => _.reduce(ast.keywords, (exists, e) => !exists ? e.text.includes(e) : true, false))
             .take(5)
+            .map((e) => e.text)
+            .join('\n')
             .value()
            );
   });
