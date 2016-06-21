59d30bc2047b717739cc6deca89b703cc8d1fa53
Lucio Franco <lucio.franco@du.edu>
2016-06-21 13:35:35 -0600

    cleaned up function names for history

--- slack.js
+++ slack.js
@@ -3,7 +3,7 @@ var token = process.env.token;
 var _ = require('lodash');
 var moment = require('moment');
 
-function history_p(channel, oldest) {
+function history(channel, oldest) {
   return new Promise((resolve, reject) => {
     slack.channels.history({token, channel, oldest: (oldest.toDate().getTime() / 1000)}, (err, data) => {
       if (err) reject(err);
@@ -12,20 +12,20 @@ function history_p(channel, oldest) {
   });
 }
 
-var a = (channel, arr) => (result) => {
+var history_recursive = (channel, arr) => (result) => {
   arr = _.concat(arr, result.messages);
   if (result.has_more) {
     var last_msg_time = result.messages[0].ts;
-    return history_p(channel, moment.unix(last_msg_time))
-      .then(a(channel, arr));
+    return history(channel, moment.unix(last_msg_time))
+      .then(history_recursive(channel, arr));
   } else {
     return arr;
   }
 };
 
 exports.history = (channel, oldest) => {
-  return history_p(channel, oldest)
-    .then(a(channel, []));
+  return history(channel, oldest)
+    .then(history_recursive(channel, []));
 };
 
 exports.im = (token, user) => {
