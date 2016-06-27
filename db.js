f6a8acb3fbeb78a1fc5ea4e86701cde7537a27e8
Lucio Franco <lucio.franco@du.edu>
2016-06-27 10:34:46 -0600

    fixed elasticsearch url and added basic schema

--- db.js
+++ db.js
@@ -1,19 +1,18 @@
 var Rebound = require('reboundodm');
 
-//Rebound.connect('localhost:9200');
+Rebound.connect('elasticsearch:9200');
 
 
-/*var messageSchema = Reboud.Schema({
-  _id: { type: Number, default: 0 },
-  message: {
-    type: Object,
-    properties: {
-      text: {
-        type: String,
-        analyzer: 'not_analyzed'
-      }
-    }
+var messageSchema = Rebound.Schema({
+  text: {
+    type: String,
+    analyzer: 'not_analyzed'
   }
-});*/
+});
 
-//var message = Reboud.modelIndex()
+var message = Rebound.modelIndex('channel', 'message', messageSchema);
+
+/*message.create({
+  text: "hello world",
+  ts: new Date().getTime() / 1000
+})*/
