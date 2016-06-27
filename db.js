c20d19e5a18848d96412e0fc82d0976ef1fa0e81
Lucio Franco <lucio.franco@du.edu>
2016-06-27 10:20:12 -0600

    added db.js and updated rebound version

---
+++ db.js
@@ -0,0 +1,19 @@
+var Rebound = require('reboundodm');
+
+//Rebound.connect('localhost:9200');
+
+
+/*var messageSchema = Reboud.Schema({
+  _id: { type: Number, default: 0 },
+  message: {
+    type: Object,
+    properties: {
+      text: {
+        type: String,
+        analyzer: 'not_analyzed'
+      }
+    }
+  }
+});*/
+
+//var message = Reboud.modelIndex()
